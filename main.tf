ebf543470272f297de44620a6e29dccf87c41966
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-05-31 22:53:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,6 @@ provider "artifactory" {
   username = "harshit.dixit@hashicorp.com"
   password = "Changeme@123"
 }
-provider_installation {
-    direct {
-        exclude = ["registry.terraform.io/*/*"]
-    }
-    network_mirror {
-        url = "https://harshithashicorp.jfrog.io/artifactory/api/terraform/harshittest-terraform-modules-local/providers/"
-    }
-}
-
 module "ec2" {
   source = "https://harshithashicorp.jfrog.io/artifactory/harshittest-terraform-modules-local/sdk/terraform-aws-ec2-instance/aws/v1.0.0.zip"
   #version = "v1.0.0"
