821a44f05397d2628194fea49143b7efd0eee7af
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-05-31 22:35:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 provider "artifactory" {
   url      = "https://harshithashicorp.jfrog.io/artifactory"
   username = "harshit.dixit@hashicorp.com"
-  password = "Changeme@123"
+  password = "cmVmdGtuOjAxOjE3NDg3MTEwNzI6TElCME90djlTa3dRdVRLb2JVaFZ2eU9Ham1C"
 }
 
 module "ec2" {
-  source = "harshithashicorp.jfrog.io/artifactory/harshittest-terraform-modules-local/sdk/terraform-aws-ec2-instance/aws/v1.0.0.zip"
+  source = "https://harshithashicorp.jfrog.io/artifactory/harshittest-terraform-modules-local/sdk/terraform-aws-ec2-instance/aws/v1.0.0.zip"
   #version = "v1.0.0"
 }
