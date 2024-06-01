e5a8f68afd5356a2d4449a99b98b013f0b48ba81
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-01 08:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ provider "artifactory" {
 }
 module "ec2" {
   source  = "harshithashicorp.jfrog.io/harshittest-terraform__sdk/terraform-aws-ec2-instance/aws"
-  version = "v1.0.0"
+  #version = "v1.0.0"
 }
