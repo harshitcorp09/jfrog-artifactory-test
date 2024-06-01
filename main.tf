441c5ff3f7ecdcec1af5bf19193724e234ee0d76
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-01 09:01:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    artifactory = {
-      source = "jfrog/artifactory"
-    }
-  }
-}
-provider "artifactory" {
-  username = "harshit.dixit@hashicorp.com"
-  password = "Changeme@123"
-}
 module "ec2" {
   source  = "harshithashicorp.jfrog.io/harshittest-terraform__sdk/terraform-aws-ec2-instance/aws"
   version = "v1.0.0"
