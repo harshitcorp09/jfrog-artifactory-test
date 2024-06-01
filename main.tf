e7e676c5faf380f49493335d026be948ae195ec5
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-01 09:07:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 module "ec2" {
   source  = "harshithashicorp.jfrog.io/harshittest-terraform__sdk/terraform-aws-ec2-instance/aws"
   version = "v1.0.0"
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
